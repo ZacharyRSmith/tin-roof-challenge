61185a4475da54e56358af72f15d67cec1d6cccc
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-24 20:53:26 -0700

    feat: implement Album creating.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,29 @@
 class AlbumsController < ApplicationController
+  def create
+    @user = User.find params[:album][:user_id]
+    @album = Album.new album_params
+    return render 'new' unless @album.save
+
+    redirect_to @user
+  end
+
   def destroy
     Album.find(params[:id]).destroy
     redirect_to root_url
+  end
+
+  def new
+    @album = Album.new
   end
 
   def show
     @album = Album.find params[:id]
     @photos = @album.photos
   end
+
+  private
+
+    def album_params
+      params.require(:album).permit(:user_id, :title)
+    end
 end
