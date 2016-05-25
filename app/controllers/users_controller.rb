2dc4f0acee78db918f935ff00bfc43f8823da06b
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-24 20:33:49 -0700

    feat: Implement User creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new user_params
+    return render 'new' unless @user.save
+
+    redirect_to root_url
+  end
+
   def index
     @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 
   def show
     @user = User.find params[:id]
     @albums = @user.albums
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :username,
+        :email,
+        :phone,
+        :website)
+    end
 end
