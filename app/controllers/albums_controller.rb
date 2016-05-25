8e76364bdc91fe7c54d14e6cf6b57854ba865b4e
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-24 20:46:20 -0700

    feat: implement Album deleting.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,9 @@
 class AlbumsController < ApplicationController
+  def destroy
+    Album.find(params[:id]).destroy
+    redirect_to root_url
+  end
+
   def show
     @album = Album.find params[:id]
     @photos = @album.photos
